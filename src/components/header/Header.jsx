dfc12d3cdef3049a8c5e6fd34572e324dd3af889
Diego Cuevas <ingdcuevas@gmail.com>
2023-03-17 10:58:23 -0500

    componente scrollup terminado, afinando detalles del header2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,8 +13,17 @@ import {
 } from "@iconscout/react-unicons";
 
 const Header = () => {
+  /* Change Background Header */
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    //when the scroll is higher than 200 vh, ad the scroll-header class to a tag with the header tag
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  });
+
   /* Toggle Menu */
   const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
 
   return (
     <header className="header">
@@ -25,37 +34,81 @@ const Header = () => {
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
+              <a
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={
+                  activeNav === "#home" ? "nav__link active-link" : "nav__link"
+                }
+              >
                 <UilEstate className="nav__icon" />
                 Home
               </a>
             </li>
             <li className="nav__item">
-              <a href="#about" className="nav__link">
+              <a
+                href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={
+                  activeNav === "#about" ? "nav__link active-link" : "nav__link"
+                }
+              >
                 <UilUser className="nav__icon" />
                 About
               </a>
             </li>
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
+              <a
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
                 <UilFileAlt className="nav__icon" />
                 Skills
               </a>
             </li>
             <li className="nav__item">
-              <a href="#services" className="nav__link">
+              <a
+                href="#services"
+                onClick={() => setActiveNav("#services")}
+                className={
+                  activeNav === "#services"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
                 <UilBriefcase className="nav__icon" />
                 Services
               </a>
             </li>
             <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
+              <a
+                href="#portfolio"
+                onClick={() => setActiveNav("#portfolio")}
+                className={
+                  activeNav === "#portfolio"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
                 <UilScenery className="nav__icon" />
                 Portfolio
               </a>
             </li>
             <li className="nav__item">
-              <a href="#contact" className="nav__link">
+              <a
+                href="#contact"
+                onClick={() => setActiveNav("#contact")}
+                className={
+                  activeNav === "#contact"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
                 <UilMessage className="nav__icon" />
                 Contact
               </a>
